cedbbfdf1f22a380374ba9aac77e720328aba0a6
Alvaro Ulloa <alvarouc@gmail.com>
2015-04-28 15:23:30 -0600

    pep8ed and pylinted simple_demo.py

--- simple_demo.py
+++ simple_demo.py
@@ -2,26 +2,33 @@
 Demo of data generation
 '''
 
-# package modules
-from generator import DataGenerator
-# classifiers
+from smri_gen.generator import DataGenerator
 import numpy as np
 
-# Prepare data
-path = '/home/aulloa/data/mega/'
-data = np.load(path + 'dataQC.npy')
-labels = np.load(path + 'labelQC.npy').ravel()
 
-# Initialize data generator with training data
-generator = DataGenerator(data, labels)
+def main():
+    '''
+    Loads data in `data` and `labels` then runs data generator on data
+    to generate new data
+    '''
+    # Prepare data
+    path = '/home/aulloa/data/mega/'
+    data = np.load(path + 'dataQC.npy')
+    labels = np.load(path + 'labelQC.npy').ravel()
 
-# generating new samples
-n_samples = 5000
-new_data= generator.generate(n_samples, method='normal')
-new_labels = [0]*n_samples + [1]n_samples
-# Saving data
-np.save(path + 'new_data.npy', new_data)
-np.save(path + 'new_labels.npy', new_labels)
+    # Initialize data generator with training data
+    generator = DataGenerator(data, labels)
+
+    # generating new samples
+    n_samples = 5000
+    new_data = generator.generate(n_samples, method='normal')
+    new_labels = [0]*n_samples + [1]*n_samples
+    # Saving data
+    np.save(path + 'new_data.npy', new_data)
+    np.save(path + 'new_labels.npy', new_labels)
+
+if __name__ == 'main':
+    main()
 
 
 
