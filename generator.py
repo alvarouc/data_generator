3a58ba8690df02d20d5f5d24ffd7e13f3c41293a
aulloa <alvarouc@gmail.com>
2015-08-04 02:07:08 -0600

    successful test for mv_rejection

--- generator.py
+++ generator.py
@@ -1,68 +1,118 @@
 '''
 This module contains the data generator class
 '''
-
-from smri_gen.rv_gen import mv_rejective
+from future.utils import implements_iterator
+from .rv_gen import mv_rejective
 from ica import ica1
 import numpy as np
 from numpy.random import multivariate_normal as mn
 
 
+@implements_iterator
+class DataGeneratorByGroup(object):
+
+    def __init__(self, data, labels,
+                 n_components=20,
+                 n_samples=100,
+                 n_batches=1000,
+                 method='normal'):
+        self.method = method
+        self.n_batches = n_batches
+        data =( data[labels==0,:], data[labels==1,:])
+        self.data_mean = (x.mean(axis=1).reshape((-1,1)) for x in data)
+        model = ica1(n_components)
+        (mix1, self.source1), (mix2, self.source2) =\
+            (model.fit(x) for x in data)
+
+        self.parameters = {
+            'sample_mean':(x.mean(axis=0) for x in [mix1,mix2]),
+            'sample_cov':(np.cov(x, rowvar=0) for x in [mix1,mix2]),
+            'sample_hist':([np.histogram(column, density=True, bins=20)[0]
+                            for column in x.T] for x in [mix1, mix2]),
+            'n_samples':n_samples}
+        
+    def __iter__(self):
+        self.batch = 0
+        return self
+
+    def __next__(self):
+
+        self.batch +=1
+        if self.batch > self.n_batches:
+            raise StopIteration
+        
+        if self.method == 'normal':
+            new_mixing0 = mn(self.parameters['sample_mean'][0],
+                             self.parameters['sample_cov'][0],
+                             self.parameters['n_samples'])
+            new_mixing1 = mn(self.parameters['sample_mean'][1],
+                             self.parameters['sample_cov'][1],
+                             self.parameters['n_samples'])
+
+        if self.method == 'rejective':
+            new_mixing0 = mv_rejective(self.parameters['sample_hist'][0],
+                                      self.parameters['n_samples'])
+            new_mixing1 = mv_rejective(self.parameters['sample_hist'][1],
+                                      self.parameters['n_samples'])
+
+        new_data0 = np.dot(new_mixing0, self.sources) + self.data_mean[0]
+        new_data1 = np.dot(new_mixing1, self.sources) + self.data_mean[1]
+        return np.vstack((new_data0,new_data1))
+    
+
+    
+@implements_iterator
 class DataGenerator(object):
     '''
     Class that generates data using ICA and a RV generator method
     '''
-    def __init__(self, data, n_components=15):
+    def __init__(self, data, 
+                 n_components=20,
+                 n_samples=100,
+                 n_batches=1000,
+                 method='normal'):
         '''
         Method that initializes the Data generator. It runs ICA on
         data with n_components
         '''
+        self.method = method
+        self.n_batches = n_batches
         # Set number of components to 15,
         # TO DO: use estimation of number of sources from data
         # pre-processing data
-        self.data_mean = data.mean(axis=0).reshape((1,-1))
-        self.data = data - self.data_mean
+        #self.data_mean = data.mean(axis=1).reshape((-1,1))
         
         # Running ICA
-        self.mixing, self.sources\
-            = ica1(self.data, n_components, verbose=False)
+        model = ica1(n_components)
+        mixing, self.sources = model.fit(data)
+
+        self.parameters = {
+            'sample_mean':np.mean(mixing, axis=0),
+            'sample_cov':np.cov(mixing, rowvar=0),
+            'sample_hist':[np.histogram(column, density=True, bins=20)
+                           for column in mixing.T],
+            'n_samples':n_samples,
+        }
 
 
-    def generate(self, n_samples, subset=None, method = 'normal'):
+    def __iter__(self):
+        self.batch=0
+        return self
+        
+    def __next__(self):
+        self.batch += 1
+        if self.batch > self.n_batches:
+            raise StopIteration
+        if self.method == 'normal':
+            new_mixing = mn(self.parameters['sample_mean'],
+                            self.parameters['sample_cov'],
+                            self.parameters['n_samples'])
 
-        if method == 'normal':
-            sample_mean = np.mean(self.mixing[subset,:], axis=0)
-            sample_cov = np.cov(self.mixing[subset,:], rowvar=0)
-            new_mixing = mn(sample_mean, sample_cov, n_samples)
+        if self.method == 'rejective':
+            new_mixing = mv_rejective(self.parameters['sample_hist'],
+                                      self.parameters['n_samples'])
 
-        if method == 'rejective':
-            model = [np.histogram(column, density=True, bins=20)
-                     for column in self.mixing[subset,:].T]
-            new_mixing = mv_rejective(model, n_samples)
-
-        new_data = np.dot(new_mixing, self.sources) + self.data_mean
+        new_data = np.dot(new_mixing, self.sources)# + self.data_mean
         return new_data
 
-    def generate_by_group(self, n_samples, labels, method='normal'):
-        '''
-        Generates samples (n_samples) with the specified method
-        Input
-         -n_samples: number of samples to generate
-         -method: 'rejective' or 'normal'
-        Output
-         -new_samples
-        '''
-        new_mixings = []
-        for tag in np.unique(labels):
-            new_mixings.append(self.generate(n_samples, labels==tag, method))
-
- 
-        n_groups = np.unique(labels).shape[0]
-        new_data = np.zeros((n_samples*n_groups,
-                             self.data.shape[1]))
-        for idx, mix in enumerate(new_mixings):
-            new_data[idx*n_samples:(idx+1)*n_samples, :]\
-                = np.dot(mix, self.sources) + self.data_mean
-
-        return new_data
-
+    
--- test/rv_gen_test.py
+++ test/rv_gen_test.py
@@ -1,9 +1,19 @@
 import unittest
-from smri_gen import mv_rejective, DataGenerator
+from data_generator import mv_rejective, DataGenerator
+from ica import ica1
 from numpy.random import multivariate_normal as mn
 from numpy.random import logistic, uniform
-from numpy import eye, histogram, allclose, dot, vstack
-
+from numpy.linalg import norm
+from numpy import (eye,
+                   histogram,
+                   allclose,
+                   dot,
+                   vstack,
+                   corrcoef,
+                   abs,sign,
+                   array)
+import theano.sandbox.cuda
+theano.sandbox.cuda.use('cpu')
 def synthetic_data():
     mixing_0 = mn([1,1,1], cov=eye(3), size = 500)
     mixing_1 = mn([-1,-1,-1], cov=eye(3), size = 500)
@@ -16,41 +26,68 @@ def synthetic_data():
     return (data,labels)
 
 class TestRVMethods(unittest.TestCase):
+
     def test_mv_rejective(self):
         # three dimensional input samples
-        input_samples = mn(mean=[0,0,0], cov=eye(3), size=10000)
-        models = [histogram(column, density=True, bins=20)\
-                  for column in input_samples.T]
-        out_samples = mv_rejective(models, 5000)
-        # Check for output size
-        self.assertEqual(out_samples.shape, (5000,3))
-        out_models = [histogram(column, density=True, bins=20)\
-                      for column in out_samples.T]
-        # Check for relative error: allow 1% missmatch
-        self.assertTrue(all([allclose(model[0], out_model[0], atol=0.01*20)\
-                             for model,out_model in zip(models,out_models)]))
+        old_data, labels = synthetic_data()
+        gen = DataGenerator(old_data, n_components=3,
+                            n_samples=1000, n_batches=10,
+                            method='rejective')
+        print('Generating new data')
+        new_data = [x for x in gen]
+        print('Done.')
 
-    def test_data_generator_ica(self):
-
-        data, labels = synthetic_data()
-        gen = DataGenerator(data, labels, n_components=3)
-        # Test if ica decomposition is ok
-        ica_data = dot(gen.mixing, gen.sources) + gen.data_mean
-        self.assertTrue(allclose(ica_data, data))
-
-    def test_data_generator_normal(self):
-        data, labels = synthetic_data()
-        gen = DataGenerator(data, labels, n_components=3)
-        # Test generator
-        new_data = gen.generate(1000, method='normal')
-        self.assertEqual(new_data.shape, (2000,50000))
+        for i in range(10):
+            self.assertEqual(new_data[i].shape[0],1000)
         
-    def test_data_generator_rejective(self):
-        data, labels = synthetic_data()
-        gen = DataGenerator(data, labels, n_components=3)
-        # Test generator
-        new_data = gen.generate(1000, method='rejective')
-        self.assertEqual(new_data.shape, (2000,50000))
+        old_model = gen.parameters['sample_hist']
+        
+        model = ica1(3)
+        # foreach data batch
+        for i in range(10):
+            new_A, new_S = model.fit(new_data[i])
+            # Reorder components
+            c_SS = corrcoef(new_S, gen.sources)[3:,:3]
+            source_sim = abs(c_SS).max(axis=1)
+            # Check sources are similar
+            self.assertTrue(all(source_sim>0.95))
+            order = abs(c_SS).argmax(axis=1)
+            signs = array([sign(x[order[n]])
+                           for n,x in enumerate(c_SS)])
+            new_S = new_S[order,:] * signs.reshape((-1,1))
+            new_A = new_A[:,order] * signs.reshape((1,-1))
+
+            new_model = [histogram(column, density=True, bins=20)\
+                         for column in new_A.T]
+            # Check that resulting new mixing has similar histogram
+            for j in range(len(new_model)):
+                sim = abs(corrcoef(new_model[j][0], old_model[j][0]))
+                self.assertTrue( sim[0,1] > 0.8,
+                                'simmilarity {} is too low'.format(sim))
+                
+
+#class TestGenerator(unittest.TestCase):       
+#    def test_data_generator_ica(self):
+#
+#        data, labels = synthetic_data()
+#        gen = DataGenerator(data, labels, n_components=3)
+#        # Test if ica decomposition is ok
+#        ica_data = dot(gen.mixing, gen.sources) + gen.data_mean
+#        self.assertTrue(allclose(ica_data, data))
+#
+#    def test_data_generator_normal(self):
+#        data, labels = synthetic_data()
+#        gen = DataGenerator(data, labels, n_components=3)
+#        # Test generator
+#        new_data = gen.generate(1000, method='normal')
+#        self.assertEqual(new_data.shape, (2000,50000))
+#        
+#    def test_data_generator_rejective(self):
+#        data, labels = synthetic_data()
+#        gen = DataGenerator(data, labels, n_components=3)
+#        # Test generator
+#        new_data = gen.generate(1000, method='rejective')
+ #       self.assertEqual(new_data.shape, (2000,50000))
         
 
 if __name__ == '__main__':
